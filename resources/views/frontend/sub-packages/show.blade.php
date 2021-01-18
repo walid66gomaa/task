e3070a4f847749089f2975cd3f02a0e8b4093c95
walid elbehery <iam66walid@gmail.com>
2021-01-18 22:11:31 +0200

    refactor

--- resources/views/frontend/sub-packages/show.blade.php
+++ resources/views/frontend/sub-packages/show.blade.php
@@ -8,20 +8,7 @@
 @endpush
 @section('content')
 
-    <!-- Start of breadcrumb section
-        ============================================= -->
-    <section id="breadcrumb" class="breadcrumb-section relative-position backgroud-style">
-        <div class="blakish-overlay"></div>
-        <div class="container">
-            <div class="page-breadcrumb-content text-center">
-                <div class="page-breadcrumb-title">
-                    <h2 class="breadcrumb-head black bold"><span>@lang('labels.frontend.subscripe')</span> </h2>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- End of breadcrumb section
-        ============================================= -->
+  
 
 
     <!-- Start of Checkout content
@@ -29,8 +16,7 @@
     <section id="checkout" class="checkout-section">
         <div class="container">
             <div class="section-title mb45 headline text-center">
-                <span class="subtitle text-uppercase">@lang('labels.frontend.cart.your_shopping_cart')</span>
-                <h2>see all products</h2>
+              <h2>see all products</h2>
             </div>
             <div class="checkout-content">
                 @if(session()->has('danger'))
@@ -41,13 +27,7 @@
                 @endif
                 <div class="row">
                     <div class="col-md-9">
-                        <div class="order-item mb30 course-page-section">
-                            <div class="section-title-2  headline text-right">
-                                <h2>@lang('labels.frontend.cart.order_item')</h2>
-                            </div>
-
-                           
-                        </div>
+                     
                     
 
 
@@ -55,13 +35,13 @@
                             @if(env('STRIPE_ACTIVE') == 0)
                                 <div class="order-payment">
                                     <div class="section-title-2  headline text-right">
-                                <h2>@lang('labels.frontend.cart.no_payment_method')</h2>
+                                <h2>No payment method available at this moment</h2>
                                     </div>
                                 </div>
                             @else
                                 <div class="order-payment">
                                     <div class="section-title-2  headline text-right">
-                                        <h2>@lang('labels.frontend.cart.order_payment')</h2>
+                                        <h2>Order <span>Payment.</span></h2>
                                     </div>
                                     <div id="accordion">
                                         @if(env('STRIPE_ACTIVE') == 1)
@@ -75,7 +55,7 @@
                                                                         <input data-toggle="collapse" href="#collapsePaymentOne"
                                                                                type="radio" name="paymentMethod" value="1"
                                                                                checked>
-                                                                        @lang('labels.frontend.cart.payment_cards')
+                                                                               Credit or Debit Card
                                                                     </label>
                                                                 </div>
                                                             </div>
@@ -107,41 +87,41 @@
 
 
                                                         <div class="payment-info">
-                                                            <label class=" control-label">@lang('labels.frontend.cart.name_on_card') :</label>
+                                                            <label class=" control-label">Name on Card :</label>
                                                             <input type="text" autocomplete='off' class="form-control required card-name"
-                                                                   placeholder="@lang('labels.frontend.cart.name_on_card_placeholder')" value="">
+                                                                   placeholder="Enter the name written on your card" value="">
                                                         </div>
                                                         <div class="payment-info">
-                                                            <label class=" control-label">@lang('labels.frontend.cart.card_number') :</label>
+                                                            <label class=" control-label">@lang('card_number') :</label>
                                                             <input autocomplete='off' type="text"
                                                                    class="form-control required card-number"
-                                                                   placeholder="@lang('labels.frontend.cart.card_number_placeholder')" value="">
+                                                                   placeholder="@lang('card number')" value="">
                                                         </div>
                                                         <div class="payment-info input-2">
-                                                            <label class=" control-label">@lang('labels.frontend.cart.cvv') :</label>
+                                                            <label class=" control-label">@lang('cvv') :</label>
                                                             <input type="text" class="form-control card-cvc required"
-                                                                   placeholder="@lang('labels.frontend.cart.cvv')"
+                                                                   placeholder="@lang('cvv')"
                                                                    value="">
                                                         </div>
                                                         <div class="payment-info input-2">
-                                                            <label class=" control-label">@lang('labels.frontend.cart.expiration_date') :</label>
+                                                            <label class=" control-label">@lang('expiration date') :</label>
                                                             <input autocomplete='off' type="text"
                                                                    class="form-control required card-expiry-month"
-                                                                   placeholder="@lang('labels.frontend.cart.mm')"
+                                                                   placeholder="@lang('mm')"
                                                                    value="">
                                                             <input autocomplete='off' type="text"
                                                                    class="form-control required card-expiry-year"
-                                                                   placeholder="@lang('labels.frontend.cart.yy')"
+                                                                   placeholder="@lang('yy')"
                                                                    value="">
                                                         </div>
                                                         <button type="submit"
                                                                 class="text-white genius-btn mt25 gradient-bg text-center text-uppercase  bold-font">
-                                                            @lang('labels.frontend.cart.pay_now') <i class="fas fa-caret-right"></i>
+                                                            @lang('pay now') <i class="fas fa-caret-right"></i>
                                                         </button>
                                                         <div class="row mt-3">
                                                             <div class="col-12 error form-group d-none">
                                                                 <div class="alert-danger alert">
-                                                                    @lang('labels.frontend.cart.stripe_error_message')
+                                                                    Please correct the errors and try again
                                                                 </div>
                                                             </div>
                                                         </div>
