c98b20a3e740033888b311fafc63e57b68ad22fb
walid elbehery <iam66walid@gmail.com>
2021-01-18 20:33:15 +0200

    refactor

--- config/app.php
+++ config/app.php
@@ -175,9 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class,
-        Spatie\Html\HtmlServiceProvider::class,
-        Yajra\DataTables\DataTablesServiceProvider::class,
 
     ],
 
